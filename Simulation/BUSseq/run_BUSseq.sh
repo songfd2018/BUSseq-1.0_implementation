e2870b70ed33acc8de0778ebd3840b53d7e8f2b4
songfd2018 <38712635+songfd2018@users.noreply.github.com>
2019-09-14 20:59:07 +0800

    Add files via upload

--- Simulation/BUSseq/run_BUSseq.sh
+++ Simulation/BUSseq/run_BUSseq.sh
@@ -4,35 +4,35 @@
 cd /your/working/directory/BUSseq_implementation-1.0/Simulation/
 
 # K = 5
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 4475 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 9123 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 3
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 7970 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 9722 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 4
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 4870 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 2088 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 6
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 579 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 9641 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 7
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 3757 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 4618 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 8
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 5340 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 7245 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 9
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 6794 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 8200 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # K = 10
-the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 184 -c 8
+the/source/code/directory/BUSseq_omp/BUSseq -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -o 2000 -s 4191 -c 8
 the/source/code/directory/BUSseq_inference -d./BUSseq/ -r./RawCountData/ -p simulation -v 1 -K 3 -i 8000 -b 4000 -c 8
 
 # Summarize the results of BUSseq
